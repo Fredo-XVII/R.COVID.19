e77a7e7f295a7737b878bf63e8abe4e11e0d15bf
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-03-30 08:24:42 -0500

    testing tidycensus for future use

--- pkg_dev/test_functions.R
+++ pkg_dev/test_functions.R
@@ -17,3 +17,14 @@ us_co_cases <- R.COVID.19::covid19_us_co()
 
 # tidycensus
 
+library(tidycensus)
+library(tidyverse)
+library(viridis)
+tarr <- get_acs(geography = "tract", variables = "B19013_001",
+                state = "TX", county = "Tarrant", geometry = TRUE)
+ggplot(tarr, aes(fill = estimate, color = estimate)) +
+  geom_sf() +
+  coord_sf(crs = 26914) +
+  scale_fill_viridis(option = "magma") +
+  scale_color_viridis(options = "magma")
+vt <- get_acs(geography = "county", variables = "B19013_001", state = "VT")
