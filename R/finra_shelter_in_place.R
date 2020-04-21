c42a1285ed9133e8e0edd255e990eab8d812ac94
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-04-20 23:35:00 -0500

    finra fx built, move to pkg build

--- R/finra_shelter_in_place.R
+++ R/finra_shelter_in_place.R
@@ -3,58 +3,53 @@
 #'
 #' @description
 #'   This functions pulls dates for shelter in place order by states with notes
-#'   and start date and end dates, as well as, extention dates.
+#'   and start date and end dates, as well as, extention dates from the FINRA
+#'   COVID-19 website. No Parameters
 #'
-#' @return A datarame/tibble
+#' @return A dataframe/tibble
 #'
 #' @examples
 #' \dontrun{
-#' confirmed <- R.COVID.19::finra_shelter_in_place()
+#' stay_orders <- R.COVID.19::finra_shelter_in_place()
 #' }
 #'
 #' @importFrom  magrittr %>%
-#' @importFrom  tidyr pivot_longer
-#' @importFrom  dplyr last
-#' @importFrom  readr read_csv
-#' @importFrom  rlang .data
+#' @importFrom  purrr map_df
+#' @importFrom  dplyr last mutate
+#' @importFrom  rvest read_html html_table html_nodes
+#' @importFrom  tibble as_tibble
 #'
 #' @export
 
 
 finra_shelter_in_place <- function() {
-  # Get confirmed cases
-  url_confirmed <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-  covid19_confirm_wide <- readr::read_csv(url(url_confirmed))
+  # Get html file
+  url_dates <- url("https://www.finra.org/rules-guidance/key-topics/covid-19/shelter-in-place")
+  webpage <- read_html(url_dates)
+  tbls <- rvest::html_nodes(webpage, "table")
+  df <- rvest::html_table(tbls[1]) %>% purrr::map_df(.f = as.data.frame)
 
-  # From wide to long
-  covid19_df <- covid19_confirm_wide %>%
-    tidyr::pivot_longer(
-      cols = .data$`1/22/20`:dplyr::last(colnames(covid19_confirm_wide)),
-      names_to = "greg_d",
-      values_to = "confirmed_cases"
+  # Clean Data
+  names(df) <- c("state", "order", "order_beg_d", "order_end_org")
+  df$order_beg_d = as.Date(df$order_beg_d, format = '%m/%d/%Y')
+  df$order_end_org <- ifelse(trimws(tolower(df$order_end_org)) == "none", NA, df$order_end_org)
+  df$order_end_org_1 = trimws(gsub("Extended to ", "", df$order_end_org))
+
+  df <- df %>%
+    dplyr::mutate(
+      order_end_d = dplyr::case_when(
+        is.na(stringr::str_match(order_end_org_1,"/")) ~
+          as.Date(paste0(stringr::str_replace(order_end_org_1," ","/"),"/2020"), format = '%B/%d/%Y'),
+        TRUE ~ as.Date(order_end_org_1,format = '%m/%d/%Y')
+      )
     )
 
-  return(invisible(covid19_df))
+  # Add Variables
+  df$order_extended = ifelse(grepl("Extended to", df$order_end_org), 1, 0)
+
+  # Remove unwanted variables
+  df$order_end_org_1 <- NULL
+
+  return(invisible(tibble::as_tibble(df)))
 }
 
-library(rvest)
-url_dates <- url("https://www.finra.org/rules-guidance/key-topics/covid-19/shelter-in-place")
-webpage <- read_html(url_dates)
-tbls <- html_nodes(webpage, "table")
-stay_at_home.df <- html_table(tbls[1]) %>% purrr::map_df(.f = as.data.frame)
-names(stay_at_home.df) <- c("state", "order", "order_beg_d", "order_end_d")
-stay_at_home.df$extended = ifelse(grepl("Extended to", stay_at_home.df$order_end_d), 1, 0)
-stay_at_home.df$end_date_1 = trimws(gsub("Extended to ", "", stay_at_home.df$order_end_d))
-stay_at_home.df <- stay_at_home.df %>% 
-  dplyr::mutate(
-    end_date = dplyr::case_when(
-      !is.na(stringr::str_match(tolower(end_date_1),"none")) ~ "",
-      #"none" %in% trimws(tolower(end_date_1)) == TRUE ~ NA,
-      is.na(stringr::str_match(end_date_1,"/")) ~ as.Date(paste0(str_replace(end_date_1," ","/"),"/2020"), format = '%B/%d/%Y'),
-      TRUE ~ as.Date(end_date_1,format = '%m/%d/%Y')
-    )
-  )
-stay_at_home.df$end_date_base <- NULL
-%B, %d, %Y
-stay_at_home.df$start_date = as.Date(stay_at_home.df$order_beg_d, format = '%m/%d/%Y')
-stay_at_home.df$end_date = as.Date(stay_at_home.df$end_date, format = '%m/%d/%Y')  #Lost the "May 3" date, need to address
