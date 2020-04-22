eeaf0239ae613d4324eb485e9811f5eab2e9a1a2
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-04-21 23:48:14 -0500

    added details, improved code

--- R/global_deaths_daily.R
+++ R/global_deaths_daily.R
@@ -7,12 +7,14 @@
 #'   data is stored within the package, so the data should continuously update
 #'   over time as long as the links do not change.
 #'
-#' @return A datarame/tibble
+#' @details
+#'   Website: "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+#'
+#' @return A dataframe/tibble
 #'
 #' @examples
-#' \dontrun{
-#' confirmed <- R.COVID.19::global_deaths_daily()
-#' }
+#' deaths <- R.COVID.19::global_deaths_daily()
+#' head(deaths)
 #'
 #' @importFrom  magrittr %>%
 #' @importFrom  tidyr pivot_longer
@@ -25,8 +27,8 @@
 
 global_deaths_daily <- function() {
 # Get death cases
-url_deaths <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
-covid19_deaths_wide <- readr::read_csv(url(url_deaths))
+url_deaths <- url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+covid19_deaths_wide <- readr::read_csv(url_deaths)
 
 covid19_df <- covid19_deaths_wide %>%
   tidyr::pivot_longer(
