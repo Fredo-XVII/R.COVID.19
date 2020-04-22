659b18027f9155a837b629f25b0592ed92103716
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-04-21 23:55:39 -0500

    added details, fixed code

--- R/pub_hlth_status_by_cnty.R
+++ R/pub_hlth_status_by_cnty.R
@@ -5,14 +5,15 @@
 #'   This functions pulls the public health status by county in the US as of the
 #'   current date.  The data comes from ESRI open data website.  The data is 1 row per
 #'   county.
-#'   source: https://coronavirus-disasterresponse.hub.arcgis.com/datasets/97792521be744711a291d10ecef33a61/data?geometry=76.921%2C-16.701%2C-109.056%2C72.161
 #'
-#' @return A datarame/tibble
+#' @details
+#'   Website: https://coronavirus-disasterresponse.hub.arcgis.com/datasets/97792521be744711a291d10ecef33a61/data?geometry=76.921%2C-16.701%2C-109.056%2C72.161
+#'
+#' @return A dataframe/tibble
 #'
 #' @examples
-#' \dontrun{
 #' pub_status_cnty <- R.COVID.19::pub_hlth_status_by_cnty()
-#' }
+#' head(pub_status_cnty)
 #'
 #' @importFrom  magrittr %>%
 #' @importFrom  readr read_csv
@@ -22,7 +23,7 @@
 
 pub_hlth_status_by_cnty <- function() {
   # Get data
-  url_csv <- "https://opendata.arcgis.com/datasets/97792521be744711a291d10ecef33a61_0.csv"
-  csv_df <- readr::read_csv(url(url_csv))
+  url_csv <- url("https://opendata.arcgis.com/datasets/97792521be744711a291d10ecef33a61_0.csv")
+  csv_df <- readr::read_csv(url_csv)
   return(invisible(csv_df))
 }
