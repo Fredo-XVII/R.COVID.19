9602914ceda4cd6152944260afcd591991e1f0f2
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-04-22 00:01:20 -0500

    added details, fixed examples

--- R/pub_hlth_status_by_cnty_shp.R
+++ R/pub_hlth_status_by_cnty_shp.R
@@ -3,16 +3,19 @@
 #'
 #' @description
 #'   This functions pulls the public health status by county in the US as of the
-#'   current date with the shapefiles.  The data comes from ESRI open data website.
-#'   The data is 1 row per county.
-#'   source: https://coronavirus-disasterresponse.hub.arcgis.com/datasets/97792521be744711a291d10ecef33a61/data?geometry=76.921%2C-16.701%2C-109.056%2C72.161
+#'   current date with the shapefiles, which are saved to the current working
+#'   directory.  The data comes from ESRI open data website.  The data is 1 row
+#'   per county.
 #'
-#' @return A datarame/tibble
+#' @details
+#'   Website: https://coronavirus-disasterresponse.hub.arcgis.com/datasets/97792521be744711a291d10ecef33a61/data?geometry=76.921%2C-16.701%2C-109.056%2C72.161
+#'
+#' @return A dataframe/tibble.  This functions has side effects in that it downloads
+#' shapefiles corresponding to the counties to the current working directory.
 #'
 #' @examples
-#' \dontrun{
 #' pub_status_cnty_shp <- R.COVID.19::pub_hlth_status_by_cnty_shp()
-#' }
+#' head(pub_status_cnty_shp)
 #'
 #' @importFrom  sf st_read
 #' @importFrom utils download.file unzip
