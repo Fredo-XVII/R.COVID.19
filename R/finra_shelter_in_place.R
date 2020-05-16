bd161d2e718fd2ad68fe209c0d4f0d219743fd4e
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-05-15 19:42:38 -0500

    fixed some of the date issues

--- R/finra_shelter_in_place.R
+++ R/finra_shelter_in_place.R
@@ -22,7 +22,7 @@
 #' @importFrom  rvest html_table html_nodes
 #' @importFrom  xml2 read_html
 #' @importFrom  tibble as_tibble
-#' @importFrom  stringr str_match str_replace
+#' @importFrom  stringr str_match str_replace str_detect
 #'
 #' @export
 
@@ -37,10 +37,10 @@ finra_shelter_in_place <- function() {
   # Clean Data
   names(df) <- c("state", "order", "order_beg_d", "order_end_org")
   df$order_beg_d <- as.Date(df$order_beg_d, format = '%m/%d/%Y')
-  df$order_end_org <- ifelse(trimws(tolower(df$order_end_org)) == "none", NA, df$order_end_org)
+  df$order_end_org_0 <- ifelse(trimws(tolower(df$order_end_org)) == "none", NA, df$order_end_org)
 
   # Remove text from dates
-  df$order_end_org_1 <- trimws(gsub("Extended to", "", df$order_end_org))
+  df$order_end_org_1 <- trimws(gsub("Extended to", "", df$order_end_org_0))
   df$order_end_org_1 <- trimws(gsub('Rescinded April 24', "4/24/2020", df$order_end_org_1 ))
   df$order_end_org_1 <- trimws(gsub("Order Rescinded", "", df$order_end_org_1 ))
   df$order_end_org_1 <- trimws(gsub("Repealed", "", df$order_end_org_1 ))
@@ -48,20 +48,37 @@ finra_shelter_in_place <- function() {
   df$order_end_org_1 <- trimws(gsub('\"Stay-At-Home\" Expired', "", df$order_end_org_1 ))
   df$order_end_org_1 <- trimws(gsub('\"Stay-at-Home\" Expired', "", df$order_end_org_1 ))
   df$order_end_org_1 <- trimws(gsub('\"Work-or-Home\" Restriction Lifted', "", df$order_end_org_1 ))
+  df$order_end_org_1 <- trimws(gsub('\"Stay-at-Home\" Superseded', "", df$order_end_org_1 ))
+  df$order_end_org_1 <- trimws(gsub('Overturned', "", df$order_end_org_1 ))
 
   df <- df %>%
     dplyr::mutate(
       order_end_d = dplyr::case_when(
-        is.na(stringr::str_match(order_end_org_1,"/")) ~
+        is.na(stringr::str_match(.data$order_end_org_1,"/")) ~
           as.Date(paste0(stringr::str_replace(order_end_org_1," ","/"),"/2020"), format = '%B/%d/%Y'),
         TRUE ~ as.Date(order_end_org_1,format = '%m/%d/%Y')
       )
+    ) %>%
+    dplyr::mutate(
+      order_end_d = case_when(lubridate::year(.data$order_end_d) != 2020 ~
+                                lubridate::ymd(2020,
+                                               lubridate::month(.data$order_end_d),
+                                               lubridate::day(.data$order_end_d)),
+                              TRUE ~ .data$order_end_d)
     )
 
   # Add Variables
   df$order_extended = ifelse(grepl("Extended to", df$order_end_org), 1, 0)
 
+  # Add reopening variable
+  df$re_opening <- stringr::str_detect(trimws(df$state), pattern = "\\*") %>%
+    as.numeric()
+
+  # Remove Asterick from state field
+  df$state <- stringr::str_replace(df$state,pattern = '\\*', "") %>% trimws()
+
   # Remove unwanted variables
+  df$order_end_org_0 <- NULL
   df$order_end_org_1 <- NULL
 
   return(invisible(tibble::as_tibble(df)))
