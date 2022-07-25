c3fb638b7541e9e041f27d8aaa3a4695127d3a66
Alfredo G Marquez <ALREDO.G.MARQUEZ@target.com>
2022-07-24 23:01:05 -0500

    fix issue with new files for function us_counties_daily

--- R/us_counties_daily.R
+++ R/us_counties_daily.R
@@ -13,6 +13,8 @@
 #'
 #'     - https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
 #'
+#'     - https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-2020.csv
+#'
 #' @return A dataframe/tibble
 #'
 #' @examples
@@ -22,15 +24,30 @@
 #' @importFrom readr read_csv
 #' @importFrom dplyr arrange
 #' @importFrom  rlang .data
+#' @importFrom  lubridate year
 #'
 #' @export
 
-
 us_counties_daily <- function() {
-  # Get confirmed cases
-  url_counties <- url("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
-  covid_df <- readr::read_csv(url_counties) %>%
-    dplyr::arrange(.data$state,.data$county,.data$date)
 
-  covid_df
+  # Set up URL over years
+  current_year <- lubridate::year(Sys.Date())
+  years <- seq(2020, current_year,1)
+  url_vector <- sprintf("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-%s.csv", years)
+
+  # Get confirmed cases
+  covid_df <- data.frame()
+
+  for (i in seq_along(url_vector)) {
+    message(sprintf("Start Data Pull for Year: %s", years[i]))
+    catch_url <- url(url_vector[i]) # Capture url for the year.
+    covid_df_base <- readr::read_csv(catch_url)
+
+    covid_df <- rbind(covid_df,covid_df_base)
+  }
+
+  # Return Sorted Data
+  covid_df %>%
+    dplyr::group_by(.data$state,.data$county,.data$date)%>%
+    dplyr::arrange(.data$state,.data$county,.data$date, .by_group = TRUE)
 }
